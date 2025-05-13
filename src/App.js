211d9d839165b21abc4071acd79e8b943cef08b0
Gregory204 <131078905+Gregory204@users.noreply.github.com>
2025-05-12 21:29:44 -0400

    minor adjusted cart!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+// Import our components
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
